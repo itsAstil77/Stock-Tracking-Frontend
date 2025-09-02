520a974bd8e5d7b56f307f6bcfadf8fd1d836f10
Pbala2002 <pbalakrishnan18082002@gmail.com>
2025-09-02 20:00:06 +0530

    today3

--- src/app/components/pages/scan-creator/scan-creator/scan-creator.ts
+++ src/app/components/pages/scan-creator/scan-creator/scan-creator.ts
@@ -170,11 +170,11 @@ export class ScanCreator {
 
     // Flexible matching for columns
     const materialCodeIndex = headers.findIndex(h =>
-      h === 'materialcode' || h === 'barcode' || h === 'material code'
+      h === 'material code' || h === 'barcode' || h === 'material code'
     );
 
     const descIndex = headers.findIndex(h =>
-      h === 'materialdescription' || h === 'description' || h === 'desc' || h === 'details'
+      h === 'material description' || h === 'description' || h === 'desc' || h === 'details'
     );
 
     if (materialCodeIndex === -1) {
